7f7c82d9ba092667662185630fa5e291477c36f7
Ashwin Oke <okeashwin@gmail.com>
2015-11-29 22:56:25 -0500

    Added randomization in host selection

--- chaos_monkey.sh
+++ chaos_monkey.sh
@@ -3,8 +3,24 @@ rno=$RANDOM
 echo "Random Number: $rno"
 PRJ_HOME=`cat config.cfg | grep PROJECT_HOME | cut -d '=' -f2`
 remainder=`expr $rno % 2`
-if [ $remainder -eq 0 ]; then
-	./restart_monkey.sh
+
+# Random number for hosts
+rHost=$RANDOM
+echo "Random Number for determining the host : $rHost"
+remainderHost=`expr $rHost % 2`
+
+# Select HOST
+HOST=""
+
+if [ $remainderHost -eq 0 ]; then
+	HOST=`cat config.cfg | grep SERVER1 | cut -d '=' -f2`
 else
-	./stress_monkey.sh
+	HOST=`cat config.cfg | grep SERVER2 | cut -d '=' -f2`
+fi
+
+# Select monkey to run
+if [ $remainder -eq 0 ]; then
+	ssh -t -o "StrictHostKeyChecking no" root@$HOST "./restart_monkey.sh"
+else
+	ssh -t -o "StrictHostKeyChecking no" root@$HOST "./stress_monkey.sh"
 fi
--- init.sh
+++ init.sh
@@ -4,21 +4,26 @@ HOST1=`cat config.cfg | grep SERVER1 | cut -d '=' -f2`
 HOST2=`cat config.cfg | grep SERVER2 | cut -d '=' -f2`
 
 # scp the monkey scripts to both the hosts
-scp restart_monkey.sh root@$HOST1:~/restart_monkey.sh
-scp stress_monkey.sh root@$HOST1:~/stress_monkey.sh
-scp chaos_monkey.sh root@$HOST1:~/chaos_monkey.sh
+scp -o "StrictHostKeyChecking no" restart_monkey.sh root@$HOST1:~/restart_monkey.sh
+scp -o "StrictHostKeyChecking no" stress_monkey.sh root@$HOST1:~/stress_monkey.sh
+scp -o "StrictHostKeyChecking no" chaos_monkey.sh root@$HOST1:~/chaos_monkey.sh
+scp -o "StrictHostKeyChecking no" config.cfg root@$HOST1:~/config.cfg
+scp -o "StrictHostKeyChecking no" stress-config root@$HOST1:~/stress-config
 
 ssh -o "StrictHostKeyChecking no" -t root@$HOST1 "chmod +x ~/restart_monkey.sh"
 ssh -o "StrictHostKeyChecking no" -t root@$HOST1 "chmod +x ~/stress_monkey.sh"
 ssh -o "StrictHostKeyChecking no" -t root@$HOST1 "chmod +x ~/chaos_monkey.sh"
 
-scp restart_monkey.sh root@$HOST2:~/restart_monkey.sh
-scp stress_monkey.sh root@$HOST2:~/stress_monkey.sh
-scp chaos_monkey.sh root@$HOST2:~/chaos_monkey.sh
+scp -o "StrictHostKeyChecking no" restart_monkey.sh root@$HOST2:~/restart_monkey.sh
+scp -o "StrictHostKeyChecking no" stress_monkey.sh root@$HOST2:~/stress_monkey.sh
+scp -o "StrictHostKeyChecking no" stress-config root@$HOST2:~/stress-config
+scp -o "StrictHostKeyChecking no" config.cfg root@$HOST2:~/config.cfg
+scp -o "StrictHostKeyChecking no" stress-config root@$HOST2:~/stress-config
 
 ssh -o "StrictHostKeyChecking no" -t root@$HOST2 "chmod +x ~/restart_monkey.sh"
 ssh -o "StrictHostKeyChecking no" -t root@$HOST2 "chmod +x ~/stress_monkey.sh"
 ssh -o "StrictHostKeyChecking no" -t root@$HOST2 "chmod +x ~/chaos_monkey.sh"
+
 
 # Prepare the cron job 
 WORKING_DIR=`pwd`
